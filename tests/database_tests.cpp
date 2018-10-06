87412dd2d77ddfd83d12c923b67435c4eb22ec38
Cristian Canedo <hackerexecute@protonmail.com>
2018-10-06 06:16:48 +0100

    Write first test_case_method:

--- tests/database_tests.cpp
+++ tests/database_tests.cpp
@@ -1,8 +1,16 @@
-#include "catch.hpp"
 #include <string>
+#include "catch.hpp"
+#include "database.h"
 
 class DatabaseTestFixture {
 private:
+    Database db;
 
 public:
+    DatabaseTestFixture() {}
 };
+
+TEST_CASE_METHOD(DatabaseTestFixture, "Establish Connection/Empty", "[database]")
+{
+    REQUIRE_THROWS(db.connect(""));
+}
